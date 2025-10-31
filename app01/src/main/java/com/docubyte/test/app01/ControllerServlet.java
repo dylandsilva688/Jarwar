7ea7e57220578a6e52e571749fa026d164504ee6
dylandsilva688 <66080245+dylandsilva688@users.noreply.github.com>
2025-10-31 18:01:54 +0530

    Update ControllerServlet.java

--- app01/src/main/java/com/docubyte/test/app01/ControllerServlet.java
+++ app01/src/main/java/com/docubyte/test/app01/ControllerServlet.java
@@ -5,12 +5,12 @@ import com.docubyte.test.app01.factory.ConnectionFactory;
 import com.docubyte.test.app01.factory.UserDaoFactory;
 import com.docubyte.test.app01.factory.UserServiceFactory;
 import com.docubyte.test.app01.service.UserService;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -201,6 +201,7 @@ public class ControllerServlet extends HttpServlet {
 
     }
 }
+
 
 
 
