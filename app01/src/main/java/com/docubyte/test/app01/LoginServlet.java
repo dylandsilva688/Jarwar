16856fafd4ba17660fe71f98578df61f87334351
dylandsilva688 <66080245+dylandsilva688@users.noreply.github.com>
2025-10-31 18:02:56 +0530

    Update LoginServlet.java

--- app01/src/main/java/com/docubyte/test/app01/LoginServlet.java
+++ app01/src/main/java/com/docubyte/test/app01/LoginServlet.java
@@ -1,10 +1,10 @@
 package com.docubyte.test.app01;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 import java.io.IOException;
@@ -48,3 +48,4 @@ public class LoginServlet extends HttpServlet {
 
     }
 }
+
